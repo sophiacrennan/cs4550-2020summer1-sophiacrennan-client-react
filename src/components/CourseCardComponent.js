703f3f7cd581a6749c19dcf58cabc4a1302da253
sophiacrennan <crennan.s@husky.neu.edu>
2020-05-26 02:35:39 -0700

    Add cards to grid layout

---
+++ src/components/CourseCardComponent.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import CourseService from "../services/CourseService";
+import {faTrash, faFileAlt, faEdit, faCheck, faImage} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default class CourseCardComponent extends React.Component{
+
+    state = {
+        editing: false,
+        course: this.props.course
+    }
+    setEditing = (editing) =>
+        this.setState({editing: editing})
+
+    ok = () =>
+        CourseService.updateCourse(
+            this.state.course._id,
+            this.state.course)
+            .then(status => this.setEditing(false))
+
+    updateCourseTitle = (newTitle) =>
+        this.setState(prevState => ({
+            course: {
+                ...prevState.course,
+                title: newTitle,
+                // owner: prevState.course.owner,
+                // modified: prevState.course.modified
+            }
+        }))
+
+
+    render() {
+         return (
+            // <div className={this.state.editing ? 'table-primary' : ''}>
+                <div className="card wbdv-card">
+                    <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap"/>
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {this.state.editing &&
+                                <input
+                                    className="form-control"
+                                    onChange={(event) => this.updateCourseTitle(event.target.value)}
+                                    value={this.state.course.title}/>
+                                } {
+                                !this.state.editing &&
+                                <Link to={`/editor/${this.state.course._id}`}>
+                                    <span className="link"> {this.state.course.title} </span>
+                                </Link>
+                            } </h5>
+                            <p className="card-text"><small className="text-muted">
+                                <FontAwesomeIcon icon={faFileAlt}></FontAwesomeIcon>
+                                {this.state.course.modified}</small>
+                            </p>
+                            <p className="float-right">
+                                {
+                                    this.state.editing &&
+                                    <button  className="btn btn-primary"
+                                             onClick={this.ok}>
+                                        <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
+                                    </button>
+                                }
+                                { !this.state.editing &&
+                                <span>
+                        <button
+                            className="btn btn-primary"
+                            onClick={() => this.setEditing(true)}>
+                            <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
+                        </button>
+                        <button
+                            className="btn btn-danger"
+                            onClick={() => this.props.deleteCourse(this.props.course)}>
+                            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                        </button>
+                    </span> }
+                            </p>
+                        </div>
+                </div>
+         )
+    }
+}
--- src/components/CourseGridComponent.js
+++ src/components/CourseGridComponent.js
@@ -1,31 +1,37 @@
 import React from "react";
+import CourseCardComponent from "./CourseCardComponent";
 import CourseRowComponent from "./CourseRowComponent";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faList, faSortAlphaDown} from "@fortawesome/free-solid-svg-icons";
 
 export default class CourseGridComponent extends React.Component {
     render() {
         return(
             <div>
-                <h3>Course Grid{this.props.courses.length}</h3>
-                <table>
-                    <thead>
-                        <th>Title</th>
-                    <th>Owner</th>
-                    <th>Last Modified</th>
-                    <th>
-                        <button>Sort</button>
-                        <button>Grid</button>
-                    </th>
-
-                    </thead>
-                    <tbody>
+                <div className="row">
+                    <div className="col-6">Recent Documents</div>
+                    <div className="col-4">Owned By Me</div>
+                    <div className="col-2">
+                        <button className="btn float-right">
+                            <FontAwesomeIcon icon={faSortAlphaDown}></FontAwesomeIcon>
+                        </button>
+                        <button className="btn wbdv-button wbdv-grid-layout wbdv-list-layout float-right"
+                                onClick={() => this.props.setLayout('table')}>
+                            <FontAwesomeIcon icon={faList}></FontAwesomeIcon>
+                        </button>
+                    </div>
+                </div>
+                <div className="card-deck">
                     {
                         this.props.courses.map(course =>
-                            <CourseRowComponent key={course._id}
-                                                course={course}/>
+                            <CourseCardComponent
+                                setLayout={this.props.setLayout}
+                                deleteCourse={this.props.deleteCourse}
+                                key={course._id}
+                                course={course}/>
                         )
                     }
-                    </tbody>
-                </table>
+                </div>
             </div>
 
         )
