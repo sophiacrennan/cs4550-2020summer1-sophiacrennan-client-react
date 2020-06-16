b8e5a35964b4c5e8cb0b59cbd106ea4ca3c32cd4
sophiacrennan <crennan.s@husky.neu.edu>
2020-06-15 21:05:33 -0700

    Implement widget component

--- src/components/WidgetListComponent.js
+++ src/components/WidgetListComponent.js
@@ -1,31 +1,109 @@
 import React from "react";
+import HeadingWidgetComponent from "./widgets/HeadingWidgetComponent";
+import ParagraphWidgetComponent from "./widgets/ParagraphWidgetComponent";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck, faPencilAlt, faPlus, faTimes, faArrowUp, faArrowDown} from "@fortawesome/free-solid-svg-icons";
 
-const WidgetListComponent = () =>
-    <div class="heading-widget">
-        <div>
-            <h2>Heading widget</h2>
-            <input
-                class="form-control"
-                id="heading-text"
-                type="text"
-                placeholder="Heading text"
-            />
-            <select class="form-control wbdv-field wbdv-dob"
-                    id="heading-type">
-                <option slected value="Heading-1">Heading 1</option>
-                <option value="Heading-2">Heading 2</option>
-                <option value="Heading-3">Heading 3</option>
-            </select>
-            <input
-                class="form-control"
-                id="widget-name"
-                type="text"
-                placeholder="Widget name"
-            />
-            <h3>Preview</h3>
-            <h1>Heading text</h1>
-        </div>
-    </div>
+class WidgetListComponent extends React.Component {
+
+    state = {
+        newWidgetTitle: 'New Topic',
+        editingWidget: {},
+        selectedWidget: {},
+    }
+
+    componentDidMount() {
+        this.props.findWidgetsForTopic(this.props.params.topicId)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.params.topicId !== this.props.params.topicId) {
+            this.props.findWidgetsForTopic(this.props.params.topicId)
+        }
+    }
+
+    updateRealWidget = (widget) => {
+        this.setState(
+            {
+                editingWidget: {}
+            }
+        )
+        this.props.updateWidget(widget.id, widget)
+    }
+
+    changeOrder = (index, changedWidget, direction) => {
+            this.props.widgets.map(widget => {
+                console.log(changedWidget.id)
+                if(widget.id === changedWidget.id) {
+                    {this.props.updateWidget(widget.id, {...widget, widgetOrder: index})}
+                }
+                else if((widget.widgetOrder === index)) {
+                    if(direction === "up") {
+                        {
+                            this.props.updateWidget(widget.id, {...widget, widgetOrder: widget.widgetOrder + 1})
+                        }
+                    } else {
+                        {this.props.updateWidget(widget.id, {...widget, widgetOrder: widget.widgetOrder - 1})}
+                    }
+                }
+            })
+       // this.props.findWidgetsForTopic(this.props.params.topicId)
+    }
+
+
+    render() {
+        return(
+            <div>
+                <h2>Widget List</h2>
+                <div>
+                    {
+                        this.props.widgets.sort((a,b) => (a.widgetOrder - b.widgetOrder)).map(widget =>
+                            <div key={widget.id} className="heading-widget">
+                                {
+                                    this.state.editingWidget.id !== widget.id &&
+                                    <button onClick={() =>
+                                        this.setState({editingWidget: widget, selectedWidget: widget})
+                                    }
+                                            className="btn btn-pill wbdv-widget-edit-btn float-right">
+                                        <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
+                                    </button>
+                                }
+                                {
+                                    widget.type === 'HEADING' &&
+                                    <HeadingWidgetComponent widget={widget}
+                                                            editing={this.state.editingWidget}
+                                                            updateRealWidget={this.updateRealWidget}
+                                                            deleteRealWidget={this.props.deleteWidget}
+                                                            changeOrder={this.changeOrder}/>
+                                }
+                                {
+                                    widget.type === 'PARAGRAPH' &&
+                                    <ParagraphWidgetComponent widget={widget}
+                                                              editing={this.state.editingWidget}
+                                                              updateRealWidget={this.updateRealWidget}
+                                                              deleteRealWidget={this.props.deleteWidget}
+                                                              changeOrder={this.changeOrder}/>
+                                }
+                            </div>)
+                    }
+                </div>
+                    <button className="btn wbdv-widget-add-btn btn-pill float-right"
+                            onClick={() =>
+                            {this.props.createWidget(
+                                this.props.params.topicId, {
+                                    type: 'HEADING',
+                                    name: 'New Widget',
+                                    size: 1,
+                                    widgetOrder: this.props.widgets.length + 1
+                                })
+                            }}>
+                        <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                    </button>
+            </div>
+        )
+    }
+}
+
 
 export default WidgetListComponent
 
